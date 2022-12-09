7121f1dd12c818e0ebc0edb1fa5b28c5e4e293c4
Joe Murphy <joe.murphy@sbab.se>
2022-12-10 00:06:22 +0100

    More days

---
+++ src/main/kotlin/adventofcode/y2015/Day17.kt
@@ -0,0 +1,18 @@
+package adventofcode.y2015 // ktlint-disable filename
+
+import adventofcode.readFile
+
+var bucketList = mutableListOf<List<Int>>()
+
+fun main(args: Array<String>) {
+    val lines = readFile("src/main/resources/y2015/day17.txt")
+    val buckets = lines.map { it.toInt() }
+    println("part1=" + fillBucket(buckets, 150))
+    println("part2=" + bucketList.filter { it.size == bucketList.minOf { min -> min.size } }.size)
+}
+
+fun fillBucket(buckets: List<Int>, space: Int, soFar: List<Int> = emptyList()): Int {
+    if (space == 0) { bucketList.add(soFar); return 1 }
+    if (space < 0) return 0
+    return buckets.mapIndexed { index, i -> fillBucket(buckets.subList(index + 1, buckets.size), space - i, soFar + i) }.sum()
+}
---
+++ src/main/kotlin/adventofcode/y2015/Day18.kt
@@ -0,0 +1,48 @@
+package adventofcode.y2015 // ktlint-disable filename
+
+import adventofcode.readFile
+
+fun main(args: Array<String>) {
+    val files = readFile("src/main/resources/y2015/day18.txt")
+    var grid = files.map { it.map { c -> c } }
+    repeat(100) { grid = gridMap(grid, brokenCorners = false) }
+    println("part1=" + grid.sumOf { it.filter { c -> c == '#' }.size })
+
+    grid = breakCorners(files.map { it.map { c -> c } })
+    repeat(100) { grid = gridMap(grid, brokenCorners = true) }
+    println("part2=" + grid.sumOf { it.filter { c -> c == '#' }.size })
+}
+
+fun breakCorners(grid: List<List<Char>>) = grid.mapIndexed { x, row -> row.mapIndexed { y, c -> if (isCorner(x) && isCorner(y)) '#' else c } }
+
+fun gridMap(grid: List<List<Char>>, brokenCorners: Boolean) = grid.mapIndexed { x, row -> List(row.size) { y -> animate(x, y, grid, brokenCorners) } }
+
+fun animate(x: Int, y: Int, grid: List<List<Char>>, brokenCorners: Boolean): Char {
+    val c = grid[x][y]
+    val surrounding = surrounding(x, y, grid)
+    return when {
+        brokenCorners && isCorner(x) && isCorner(y) -> '#'
+        c == '#' && surrounding in 2..3 -> '#'
+        c == '.' && surrounding == 3 -> '#'
+        else -> '.'
+    }
+}
+
+fun surrounding(x: Int, y: Int, grid: List<List<Char>>): Int {
+    val dir = listOf(-1, 0, 1)
+    var num = 0
+    dir.forEach { xOffset ->
+        dir.forEach { yOffset ->
+            when {
+                xOffset == 0 && yOffset == 0 -> Unit
+                (x + xOffset).let { it < 0 || it >= 100 } -> Unit
+                (y + yOffset).let { it < 0 || it >= 100 } -> Unit
+                grid[x + xOffset][y + yOffset] == '#' -> num++
+                else -> Unit
+            }
+        }
+    }
+    return num
+}
+
+fun isCorner(c: Int) = c == 0 || c == 99
