80fcfc5e1ed9c172dde1c34ba79089bbd7e7d526
Joe Murphy <joe.murphy@sbab.se>
2022-12-09 10:19:40 +0100

    More days

--- src/main/kotlin/adventofcode/Common.kt
+++ src/main/kotlin/adventofcode/Common.kt
@@ -1,6 +1,8 @@
 package adventofcode
 
 import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
 
 fun readFile(fileName: String): List<String> = File(fileName).useLines { it.toList() }
 
@@ -10,3 +12,7 @@ fun matches(line: String, regex: String) = Regex(regex)
     .findAll(line)
     .map { it.groupValues[0] }
     .toList()
+
+fun md5(input: String) = BigInteger(1, MessageDigest.getInstance("MD5").digest(input.toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
--- src/main/kotlin/adventofcode/y2015/Day4.kt
+++ src/main/kotlin/adventofcode/y2015/Day4.kt
@@ -1,8 +1,7 @@
 package adventofcode.y2015 // ktlint-disable filename
 
+import adventofcode.md5
 import adventofcode.readFile
-import java.math.BigInteger
-import java.security.MessageDigest
 
 fun main(args: Array<String>) {
     val line = readFile("src/main/resources/y2015/day4.txt")[0]
@@ -14,12 +13,6 @@ fun findMd5(line: String, numZeroes: Int): Int {
     repeat(numZeroes) { prefix += "0" }
     var i = 1
     while (true) {
-        if (md5(line + i).startsWith(prefix)) return i
-        i++
+        if (md5(line + i).startsWith(prefix)) return i else i++
     }
-}
-
-fun md5(input: String): String {
-    val md = MessageDigest.getInstance("MD5")
-    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
 }
