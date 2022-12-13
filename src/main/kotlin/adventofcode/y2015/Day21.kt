375db8a8cd6df2589c8b39012a0b0c31275a3ae6
Joe Murphy <joe.murphy@sbab.se>
2022-12-13 08:47:21 +0100

    More days

--- src/main/kotlin/adventofcode/y2015/Day21.kt
+++ src/main/kotlin/adventofcode/y2015/Day21.kt
@@ -9,16 +9,16 @@ import kotlin.math.max
 fun main(args: Array<String>) {
     val lines = readFile("src/main/resources/y2015/day21.txt")
     val boss = (0..2).map { matches(lines[it], "[0-9]+").first().toInt() }.let { Fighter(it[0], it[1], it[2]) }
-    println("part1="+iterateFight { you, cost -> if (winFight(you, boss)) minOfNull(cost, you.cost)  else cost })
-    println("part2="+iterateFight { you, cost -> if (!winFight(you, boss)) maxOfNull(cost, you.cost) else cost })
+    println("part1=" + iterateFight { you, cost -> if (winFight(you, boss)) minOfNull(cost, you.cost) else cost })
+    println("part2=" + iterateFight { you, cost -> if (!winFight(you, boss)) maxOfNull(cost, you.cost) else cost })
 }
 
 private fun iterateFight(func: (Fighter, Int?) -> Int?): Int? {
     val emptyItem = Item(0, 0, 0)
-    val weapons = listOf(8, 10, 25, 40, 74).mapIndexed { index, i ->  Item(i, index + 4, 0)}
-    val armour = listOf(13, 31, 53, 75, 102).mapIndexed { index, i ->  Item(i, 0, index + 1)} + emptyItem
-    val rings = listOf(25, 50, 100).mapIndexed { index, i ->  Item(i, index + 1, 0)} +
-            listOf(20, 40, 80).mapIndexed { index, i ->  Item(i, 0, index + 1)} + emptyItem + emptyItem
+    val weapons = listOf(8, 10, 25, 40, 74).mapIndexed { index, i -> Item(i, index + 4, 0) }
+    val armour = listOf(13, 31, 53, 75, 102).mapIndexed { index, i -> Item(i, 0, index + 1) } + emptyItem
+    val rings = listOf(25, 50, 100).mapIndexed { index, i -> Item(i, index + 1, 0) } +
+        listOf(20, 40, 80).mapIndexed { index, i -> Item(i, 0, index + 1) } + emptyItem + emptyItem
 
     val you = Fighter(100, 0, 0)
     var coins: Int? = null
@@ -36,11 +36,11 @@ private fun iterateFight(func: (Fighter, Int?) -> Int?): Int? {
     }
     return coins
 }
-private data class Item (val cost: Int, val damage: Int, val armour: Int)
+private data class Item(val cost: Int, val damage: Int, val armour: Int)
 private data class Fighter(val hp: Int, val damage: Int, val armour: Int, val cost: Int = 0) {
     fun addItem(item: Item) = copy(damage = damage + item.damage, armour = armour + item.armour, cost = cost + item.cost)
 }
-private fun winFight(you: Fighter, boss: Fighter ): Boolean {
+private fun winFight(you: Fighter, boss: Fighter): Boolean {
     val newBoss = dealDamage(you, boss).also { if (it.hp <= 0) return true }
     val newYou = dealDamage(boss, you).also { if (it.hp <= 0) return false }
     return winFight(newYou, newBoss)
