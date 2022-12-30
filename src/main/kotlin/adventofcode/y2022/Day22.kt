b52f5182433cdb8d35e890c98b19142e74ab7139
Joe Murphy <joe.murphy@sbab.se>
2022-12-30 17:18:23 +0100

    day22 incomplete

--- src/main/kotlin/adventofcode/y2022/Day22.kt
+++ src/main/kotlin/adventofcode/y2022/Day22.kt
@@ -3,69 +3,148 @@ package adventofcode.y2022 // ktlint-disable filename
 import adventofcode.matches
 import adventofcode.readFile
 
+/*private val cubeFaces = mapOf(
+    '1' to Pair(Pair(0, 8), Pair(3, 11)),
+    '2' to Pair(Pair(4, 0), Pair(7, 3)),
+    '3' to Pair(Pair(4, 4), Pair(7, 7)),
+    '4' to Pair(Pair(4, 8), Pair(7, 11)),
+    '5' to Pair(Pair(8, 8), Pair(11, 11)),
+    '6' to Pair(Pair(8, 12), Pair(11, 15))
+)*/
+
+private val cubeFaces = mapOf(
+    '1' to Pair(Pair(0, 50), Pair(49, 99)),
+    '2' to Pair(Pair(0, 100), Pair(49, 149)),
+    '3' to Pair(Pair(50, 50), Pair(99, 99)),
+    '4' to Pair(Pair(100, 0), Pair(149, 49)),
+    '5' to Pair(Pair(100, 50), Pair(149, 99)),
+    '6' to Pair(Pair(150, 0), Pair(199, 49))
+)
+
+// Part2 incomplete
 fun main(args: Array<String>) {
     val lines = readFile("src/main/resources/y2022/day22.txt")
 
-    val grid = lines.filter { it.isNotEmpty() && it.contains('.')}.map { s -> s.map { c -> Space.fromChar(c) } }
+    val grid = lines.filter { it.isNotEmpty() && it.contains('.') }.map { s -> s.map { c -> Space.fromChar(c) } }
     val maxWidth = grid.maxOf { it.size }
     val paddedGrid = grid.map {
         if (it.size == maxWidth) it
         else it + List(maxWidth - it.size) { Space.EMPTY }
     }
-
-    var facing = Facing.UP
-    var pos = 0 to paddedGrid.first().indexOf(Space.PATH)
-    matches("R" + lines.last(), "[R|L][0-9]+").forEach {
-        facing = when(it.first()) {
-            'R' -> facing.turnRight()
-            else -> facing.turnLeft()
-        }
-        val spacesToMove = it.substring(1, it.length).toInt()
-        println("$facing $spacesToMove")
-        repeat(spacesToMove) {
-            val newPos = move(pos, facing, paddedGrid)
-            if (paddedGrid[newPos.first][newPos.second] == Space.PATH) pos = newPos
-        }
-        println(pos.copy(first = pos.first + 1, second = pos.second + 1))
-    }
-    val part1 = 1000 * (pos.first + 1) + 4 * (pos.second + 1) + facing.value
-    println("part1=$part1")
+    //println("part1=" + iterate(paddedGrid, lines, isPart1 = true))
+    println("part2=" + iterate(paddedGrid, lines, isPart1 = false))
 }
 
-private fun move(pos: Pair<Int, Int>, facing: Facing, grid: List<List<Space>>): Pair<Int, Int> {
-    val newPos = facing.move(pos)
+private fun iterate(grid: List<List<Space>>, lines: List<String>, isPart1: Boolean): Int {
+    var pos = Position(0, grid.first().indexOf(Space.PATH), Facing.UP)
+    matches("R" + lines.last(), "[R|L][0-9]+").forEach {
+        pos = when (it.first()) {
+            'R' -> pos.turnRight()
+            else -> pos.turnLeft()
+        }
+        val spacesToMove = it.substring(1, it.length).toInt()
+        println("${pos.facing} $spacesToMove")
+        repeat(spacesToMove) {
+            val newPos = if (isPart1) movePart1(pos, grid) else movePart2(pos, grid)
+            if (grid[newPos.row][newPos.col] == Space.PATH) pos = newPos
+        }
+        println(pos.copy(row = pos.row + 1, col = pos.col + 1))
+    }
+    return 1000 * (pos.row + 1) + 4 * (pos.col + 1) + pos.facing.value
+}
+
+private data class Position(val row: Int, val col: Int, val facing: Facing) {
+    fun turnLeft() = when (facing) {
+        Facing.UP -> copy(facing = Facing.LEFT)
+        Facing.RIGHT -> copy(facing = Facing.UP)
+        Facing.DOWN -> copy(facing = Facing.RIGHT)
+        Facing.LEFT -> copy(facing = Facing.DOWN)
+    }
+
+    fun turnRight() = when (facing) {
+        Facing.UP -> copy(facing = Facing.RIGHT)
+        Facing.RIGHT -> copy(facing = Facing.DOWN)
+        Facing.DOWN -> copy(facing = Facing.LEFT)
+        Facing.LEFT -> copy(facing = Facing.UP)
+    }
+
+    fun move() = when (facing) {
+        Facing.UP -> copy(row = row - 1)
+        Facing.RIGHT -> copy(col = col + 1)
+        Facing.DOWN -> copy(row = row + 1)
+        Facing.LEFT -> copy(col = col - 1)
+    }
+
+    fun cubeFace() = cubeFaces.filter {
+        it.value.first.first <= row && it.value.second.first >= row &&
+            it.value.first.second <= col && it.value.second.second >= col
+    }.keys.first()
+
+    /*fun cubeFaceMove(grid: List<List<Space>>): Position {
+        val cubeFace = cubeFace()
+        return when {
+            cubeFace == '1' && facing == Facing.UP -> copy(row = grid.size - 1)
+            cubeFace == '1' && facing == Facing.RIGHT -> copy(row = 11 - row, col = 15, facing = Facing.LEFT)
+            cubeFace == '1' && facing == Facing.LEFT -> copy(row = 4, col = row + 4, facing = Facing.DOWN)
+
+            cubeFace == '3' && facing == Facing.UP -> copy(row = col - 4, col = 8, facing = Facing.RIGHT)
+            cubeFace == '4' && facing == Facing.RIGHT -> copy(row = 8, col = 19 - row, facing = Facing.DOWN)
+            cubeFace == '5' && facing == Facing.DOWN -> copy(row = 7, col = 11 - col, facing = Facing.UP)
+            else -> throw IllegalArgumentException("No support from $this")
+        }
+    }*/
+
+    fun cubeFaceMove(): Position {
+        val cubeFace = cubeFace()
+        return when {
+            cubeFace == '1' && facing == Facing.UP -> copy(row = col + 100, col = 0, facing = Facing.RIGHT)
+            cubeFace == '1' && facing == Facing.LEFT -> copy(row = 149 - row, col = 0, facing = Facing.RIGHT)
+
+            cubeFace == '2' && facing == Facing.UP -> copy(row = 199, col = col - 100, facing = Facing.UP)
+            cubeFace == '2' && facing == Facing.RIGHT -> copy(row = 149 - row, col = 99, facing = Facing.LEFT)
+            cubeFace == '2' && facing == Facing.DOWN -> copy(row = col, col = 99, facing = Facing.LEFT)
+
+            cubeFace == '3' && facing == Facing.LEFT -> copy(row = 100, col = row - 50, facing = Facing.DOWN)
+            cubeFace == '3' && facing == Facing.RIGHT -> copy(row = 49, col = row, facing = Facing.UP)
+
+            cubeFace == '4' && facing == Facing.UP -> copy(row = col + 50, col = 50, facing = Facing.RIGHT)
+            cubeFace == '4' && facing == Facing.LEFT -> copy(row = 149 - row, col = 50, facing = Facing.RIGHT)
+
+            cubeFace == '5' && facing == Facing.DOWN -> copy(row = col + 100, col = 49, facing = Facing.LEFT)
+            cubeFace == '5' && facing == Facing.RIGHT -> copy(row = 149 - row, col = 149, facing = Facing.LEFT)
+
+            cubeFace == '6' && facing == Facing.LEFT -> copy(row = 0, col = row - 100, facing = Facing.DOWN)
+            cubeFace == '6' && facing == Facing.RIGHT -> copy(row = 149, col = row - 100, facing = Facing.UP)
+            cubeFace == '6' && facing == Facing.DOWN -> copy(row = 0, col = col + 100, facing = Facing.DOWN)
+            else -> throw IllegalArgumentException("No support from $this")
+        }
+    }
+}
+
+private fun movePart1(pos: Position, grid: List<List<Space>>): Position {
+    val newPos = pos.move()
     return when {
-        newPos.first < 0 -> move(newPos.copy(first = grid.size), facing, grid)
-        newPos.first >= grid.size -> move(newPos.copy(first = -1), facing, grid)
-        newPos.second < 0 -> move(newPos.copy(second = grid.first().size), facing, grid)
-        newPos.second >= grid.first().size -> move(newPos.copy(second = -1), facing, grid)
-        grid[newPos.first][newPos.second] == Space.EMPTY -> move(newPos, facing, grid)
+        newPos.row < 0 -> movePart1(newPos.copy(row = grid.size), grid)
+        newPos.row >= grid.size -> movePart1(newPos.copy(row = -1), grid)
+        newPos.col < 0 -> movePart1(newPos.copy(col = grid.first().size), grid)
+        newPos.col >= grid.first().size -> movePart1(newPos.copy(col = -1), grid)
+        grid[newPos.row][newPos.col] == Space.EMPTY -> movePart1(newPos, grid)
+        else -> newPos
+    }
+}
+
+private fun movePart2(pos: Position, grid: List<List<Space>>): Position {
+    val newPos = pos.move()
+    return when {
+        newPos.row < 0 || newPos.row >= grid.size -> pos.cubeFaceMove()
+        newPos.col < 0 || newPos.col >= grid.first().size -> pos.cubeFaceMove()
+        grid[newPos.row][newPos.col] == Space.EMPTY -> pos.cubeFaceMove()
         else -> newPos
     }
 }
 
 private enum class Facing(val value: Int) {
-    UP(3) {
-        override fun turnLeft() = LEFT
-        override fun turnRight() = RIGHT
-        override fun move(pos: Pair<Int, Int>) = pos.copy(first = pos.first - 1)
-    }, RIGHT(0) {
-        override fun turnLeft() = UP
-        override fun turnRight() = DOWN
-        override fun move(pos: Pair<Int, Int>) = pos.copy(second = pos.second + 1)
-    }, DOWN(1) {
-        override fun turnLeft() = RIGHT
-        override fun turnRight() = LEFT
-        override fun move(pos: Pair<Int, Int>) = pos.copy(first = pos.first + 1)
-    }, LEFT(2) {
-        override fun turnLeft() = DOWN
-        override fun turnRight() = UP
-        override fun move(pos: Pair<Int, Int>) = pos.copy(second = pos.second - 1)
-    };
-
-    abstract fun turnLeft(): Facing
-    abstract fun turnRight(): Facing
-    abstract fun move(pos: Pair<Int, Int>): Pair<Int, Int>
+    UP(3), RIGHT(0), DOWN(1), LEFT(2);
 }
 
 private enum class Space {
