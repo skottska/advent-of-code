93acf77e9601b1356d3a65d588b572ab02428c16
skottska <skottska@gmail.com>
2023-01-13 22:50:41 +0100

    day4

---
+++ src/main/kotlin/adventofcode/y2020/Day04.kt
@@ -0,0 +1,24 @@
+package adventofcode.y2020 // ktlint-disable filename
+
+import adventofcode.matchNumbers
+import adventofcode.readFile
+
+fun main(args: Array<String>) {
+    val lines = readFile("src/main/resources/y2020/day04.txt").fold(listOf("")) { total, i ->
+        if (i.isEmpty()) total + listOf("") else total.last().let { total.dropLast(1) + listOf("$it $i") }
+    }
+    val validFields = mapOf<String, (String) -> Boolean>(
+        "byr" to { s -> s.toInt() in 1920..2002 },
+        "iyr" to { s -> s.toInt() in 2010..2020 },
+        "eyr" to { s -> s.toInt() in 2020..2030 },
+        "hgt" to { s -> matchNumbers(s).first().let { if (s.contains("cm")) it in 150..193 else it in 59..76 } },
+        "hcl" to { s -> s.matches(Regex("#[0-9a-f]{6}")) },
+        "ecl" to { s -> s in listOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth") },
+        "pid" to { s -> s.matches(Regex("[0-9]{9}")) },
+        "cid" to { true }
+    )
+    val passports = lines.map { line -> line.split(" ").associate { field -> field.split(":").let { it.first() to it.last() } } }
+        .filter { it.keys.containsAll(validFields.keys - "cid") }
+    println("part1=" + passports.count())
+    println("part2=" + passports.count { p -> p.all { field -> validFields[field.key]?.let { it(field.value) } ?: true } })
+}
