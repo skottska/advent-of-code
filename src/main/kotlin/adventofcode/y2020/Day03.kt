bd6f3ebe587c7eb16a51036cb281086e795404b8
skottska <skottska@gmail.com>
2023-01-13 21:52:00 +0100

    day3

---
+++ src/main/kotlin/adventofcode/y2020/Day03.kt
@@ -0,0 +1,25 @@
+package adventofcode.y2020 // ktlint-disable filename
+
+import adventofcode.Coord
+import adventofcode.readFile
+
+fun main(args: Array<String>) {
+    println("part1=" + slope(right = 3, down = 1))
+    println("part2=" + listOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2).map { slope(it.first, it.second) }.fold(1L) { total, i -> total * i })
+}
+
+private fun slope(right: Int, down: Int): Long {
+    val lines = readFile("src/main/resources/y2020/day03.txt").mapIndexed { rowIndex, row ->
+        row.mapIndexed { colIndex, col ->
+            Coord(rowIndex, colIndex) to (col == '#')
+        }
+    }
+    val puzzleMap = lines.flatten().toMap()
+    var cur = Coord(0, 0)
+    var trees = 0L
+    while (cur.row < lines.size) {
+        if (puzzleMap.getValue(cur)) trees++
+        cur = Coord(cur.row + down, (cur.col + right) % lines.first().size)
+    }
+    return trees
+}
