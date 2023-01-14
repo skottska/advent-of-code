4180752b3570772a82cf267aaae2eb32bad93ce7
skottska <skottska@gmail.com>
2023-01-14 17:30:25 +0100

    day5

---
+++ src/main/kotlin/adventofcode/y2018/Day05.kt
@@ -0,0 +1,24 @@
+package adventofcode.y2018 // ktlint-disable filename
+
+import adventofcode.readFile
+
+fun main(args: Array<String>) {
+    val line = readFile("src/main/resources/y2018/day05.txt")[0].toList()
+    println("part1=" + iterate(line))
+    println("part2=" + ('a'..'z').minOf { iterate(line.filter { l -> l.lowercase().first() != it }) })
+}
+
+private fun iterate(cs: List<Char>): Int {
+    var cur = cs
+    while (true) {
+        val newCurEven = removePairs(cur)
+        val newCurOdd = listOf(newCurEven.first()) + removePairs(newCurEven.drop(1))
+        if (newCurOdd == cur) break else cur = newCurOdd
+    }
+    return cur.size
+}
+
+private fun removePairs(cs: List<Char>) = cs.chunked(2).mapNotNull {
+    if (it.size == 2 && it.first() != it.last() && it.first().lowercase() == it.last().lowercase()) null else it
+}.flatten()
+
