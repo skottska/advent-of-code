c71c65db56ff8cffbddcd317c85559afae360a99
skottska <skottska@gmail.com>
2023-01-30 23:48:54 +0100

    y2020 day05

---
+++ src/main/kotlin/adventofcode/y2020/Day05.kt
@@ -0,0 +1,23 @@
+package adventofcode.y2020 // ktlint-disable filename
+
+import adventofcode.readFile
+
+fun main() {
+    val lines = readFile("src/main/resources/y2020/day05.txt")
+    val seats = lines.map { findRow(it) to findCol(it) }
+    println("part1=" + seats.map { it.first * 8 + it.second }.max())
+    val myRow = seats.groupBy { it.first }.map { seat -> seat.key to seat.value.map { it.second } }.filter { it.second.size != 8 }[1]
+    val myCol = (0..7).first { it !in myRow.second }
+    println("part2=" + (myRow.first * 8 + myCol))
+}
+
+private fun findRow(s: String) = findPos(l = s.filter { it == 'B' || it == 'F' }.map { it == 'F' }, max = 127)
+private fun findCol(s: String) = findPos(l = s.filter { it == 'L' || it == 'R' }.map { it == 'L' }, max = 7)
+
+private fun findPos(l: List<Boolean>, min: Int = 0, max: Int): Int {
+    return when {
+        max == min -> max
+        l.first() -> findPos(l.drop(1), min, (1 + max - min) / 2 + min - 1)
+        else -> findPos(l.drop(1), (1 + max - min) / 2 + min, max)
+    }
+}
