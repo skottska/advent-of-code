4067a913d846f798fae489e3fbbdbfa81fa9b611
Joe Murphy <joe.murphy@sbab.se>
2023-12-05 15:56:03 +0100

    2023 Day05

--- src/main/kotlin/adventofcode/y2023/Day05.kt
+++ src/main/kotlin/adventofcode/y2023/Day05.kt
@@ -12,17 +12,28 @@ fun main() {
     println("part2=" + minConversion(lines, seeds.windowed(size = 2, step = 2) { it.first()..it.first() + it.last() }))
 }
 
-private fun overlaps(a: LongRange, b: LongRange) = overlap(a, b) != null
-private fun overlap(a: LongRange, b: LongRange): LongRange? {
-    val min = max(a.first, b.first)
-    val max = min(a.last, b.last)
-    return when {
-        min > max -> null
-        else -> min..max
-    }
-}
-
 private fun move(a: LongRange, move: Long) = a.first + move..a.last + move
+
+private fun minConversion(lines: List<String>, seeds: List<LongRange>): Long {
+    val blankLines = lines.mapIndexed { i, line -> i to line }.filter { it.second.isEmpty() }.map { it.first } + lines.size
+    val conversions = blankLines.windowed(size = 2).map { c ->
+        lines.subList(c.first() + 2, c.last()).map {
+            val nums = matchNumbersLong(it)
+            (nums[1] until nums[1] + nums[2]) to nums[0]
+        }
+    }
+    return conversions.fold(seeds) { total, conversionRound ->
+        val totalM = total.toMutableList()
+        val converted = mutableListOf<LongRange>()
+        conversionRound.forEach { conv ->
+            totalM.filter { overlaps(conv.first, it) }.forEach { seed ->
+                overlap(seed, conv.first)?.let { converted.add(move(it, conv.second - conv.first.first)) }
+                totalM.remove(seed); totalM.addAll(removeFromRange(seed, conv.first))
+            }
+        }
+        totalM + converted
+    }.minOf { it.first }
+}
 
 private fun removeFromRange(a: LongRange, b: LongRange) = when {
     a.first >= b.first && a.last <= b.last -> emptyList()
@@ -31,25 +42,12 @@ private fun removeFromRange(a: LongRange, b: LongRange) = when {
     else -> listOf(b.last + 1..a.last)
 }
 
-private fun minConversion(lines: List<String>, inSeeds: List<LongRange>, inPos: Int = 1): Long {
-    val seeds = inSeeds.toMutableList()
-    val conversions = mutableListOf<Pair<LongRange, Long>>()
-    var pos = inPos + 2
-    while (pos < lines.size && lines[pos].isNotEmpty()) {
-        val nums = matchNumbersLong(lines[pos++])
-        conversions += (nums[1] until nums[1] + nums[2]) to nums[0]
-    }
-    return when (conversions.isEmpty()) {
-        true -> seeds.minOf { it.first }
-        false -> {
-            val converted = mutableListOf<LongRange>()
-            conversions.forEach { conv ->
-                seeds.filter { overlaps(conv.first, it) }.forEach { seed ->
-                    overlap(seed, conv.first)?.let { converted.add(move(it, conv.second - conv.first.first)) }
-                    seeds.remove(seed); seeds.addAll(removeFromRange(seed, conv.first))
-                }
-            }
-            minConversion(lines, seeds + converted, pos)
-        }
+private fun overlaps(a: LongRange, b: LongRange) = overlap(a, b) != null
+private fun overlap(a: LongRange, b: LongRange): LongRange? {
+    val min = max(a.first, b.first)
+    val max = min(a.last, b.last)
+    return when {
+        min > max -> null
+        else -> min..max
     }
 }
