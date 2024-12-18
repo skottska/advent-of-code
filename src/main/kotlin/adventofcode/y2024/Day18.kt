eb7fc2fe33930eddc1107045df3f5261a877cfcc
jmur <joe.murphy@sbab.se>
2024-12-18 08:28:36 +0100

    year 2024 day18

--- src/main/kotlin/adventofcode/y2024/Day18.kt
+++ src/main/kotlin/adventofcode/y2024/Day18.kt
@@ -14,24 +14,24 @@ fun main() {
 
     val start = Coord(0, 0)
     val finish = Coord(70, 70)
-    println("part1="+navigate(coords.take(1024).associateWith { '"' }, start, finish, 0))
+    println("part1="+navigate(coords.take(1024).toSet(), start, finish, 0))
 
     var min = 1024
     var max = coords.size
     while (max - min > 1) {
         val next = min + (max - min) / 2
         cache.clear()
-        val nextResult = navigate(coords.take(next).associateWith { '"' }, start, finish, 0)
+        val nextResult = navigate(coords.take(next).toSet(), start, finish, 0)
         if (nextResult == null) max = next else min = next
     }
     println("part2="+coords[max-1].let { ""+it.col+","+it.row })
 }
 
 private val cache = mutableMapOf<Coord, Int>()
-private fun navigate(grid: Map<Coord, Char>, cur: Coord, end: Coord, moves: Int): Int? {
+private fun navigate(obstacles: Set<Coord>, cur: Coord, end: Coord, moves: Int): Int? {
     cache[cur]?.let { if (it <= moves) return null }
     cache[cur] = moves
     if (cur == end) return moves
-    return cur.around().filter { grid[it] == null && listOf(it.col, it.row).all { x -> x in 0..end.col } }
-        .mapNotNull { navigate(grid, it, end, moves + 1) }.minOrNull()
+    return cur.around().filter { it !in obstacles && listOf(it.col, it.row).all { x -> x in 0..end.col } }
+        .mapNotNull { navigate(obstacles, it, end, moves + 1) }.minOrNull()
 }
